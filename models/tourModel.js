0823441cbb080797627c3eeeba04db491c9da8fb
Farzin Jabbary <farzin.jabbary@gmail.com>
2023-11-26 21:13:39 -0800

    prevent duplicate review

--- models/tourModel.js
+++ models/tourModel.js
@@ -33,7 +33,8 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         default: 4.5,
         min: [1, 'Rating must be above 1.0'],
-        max: [5, 'Rating must be below 5.0']
+        max: [5, 'Rating must be below 5.0'],
+        set: val => val.toFixed(1)
     },
     ratingsQuantity: {
         type: Number,
